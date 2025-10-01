bd162f71e78ede5345fc6e4d462e307fbbe864de
Naeem Ahmed <naeem58902@gmail.com>
2025-10-01 08:42:18 +0000

    change rain cdn to this cdn
    https://cdn.jsdelivr.net/gh/naeem5877/audio

--- src/lib/sounds.ts
+++ src/lib/sounds.ts
@@ -11,7 +11,7 @@ export type Sound = {
 };
 
 export const sounds: Sound[] = [
-  { id: 'rain', name: 'Rain', Icon: CloudRain, audioSrc: 'https://cdn.jsdelivr.net/gh/naeem5877/audiocdn@main/Calm_rainfall_agains.wav' },
+  { id: 'rain', name: 'Rain', Icon: CloudRain, audioSrc: 'https://cdn.jsdelivr.net/gh/naeem5877/audiocdn@main/Rain_Umbrella.wav' },
   { id: 'campfire', name: 'Campfire', Icon: Flame, audioSrc: 'https://cdn.jsdelivr.net/gh/naeem5877/audiocdn@main/Crackling_campfire.wav' },
   { id: 'train', name: 'Train', Icon: TrainFront, audioSrc: 'https://cdn.jsdelivr.net/gh/naeem5877/audiocdn@main/Interior_train_ambie.wav' },
   { id: 'wind', name: 'Wind', Icon: Wind, audioSrc: 'https://cdn.jsdelivr.net/gh/naeem5877/audiocdn@main/Strong_gusty_wind.wav' },
